43aca77d8cd6a1b25e689a500ede9e539eb16069
Nginacloud <nginadorcas451@gmail.com>
2023-07-13 13:27:05 -0700

    Multichanged

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,4 +3,8 @@
  * main - Prints "Programming is like building a multilingual puzzle"
  *return - return 0 if positive 
  */
-int main () { puts("Programming is like building a multilingual puzzle, \n") return (0)}
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle, \n");
+	return (0);
+}
