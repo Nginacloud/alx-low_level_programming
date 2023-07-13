b5d10cb52e6b2d711fd88befd8697fbc77246bbb
Nginacloud <nginadorcas451@gmail.com>
2023-07-13 13:15:24 -0700

    Multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+/**
+ * main - Prints "Programming is like building a multilingual puzzle"
+ *return - return 0 if positive 
+ */
+int main () { puts("Programming is like building a multilingual puzzle, \n") return (0)}
