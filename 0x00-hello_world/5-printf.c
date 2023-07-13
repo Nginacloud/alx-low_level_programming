026f72e4b00043f3a0b122fcc8f4e2fa3680690f
Nginacloud <nginadorcas451@gmail.com>
2023-07-13 14:17:09 -0700

    Hello, printf changed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints exactly "with proper grammar, but the outcome is a piece of art,"
+ * main - prints exactly "with proper grammar, 
+ * but the outcome is a piece of art,"
  * Return: 0 if positive. non-zero otherwise
  */
 int main(void)
