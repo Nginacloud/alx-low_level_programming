35d91514dcae14d516f49ea1c0032695bfcd7087
Nginacloud <nginadorcas451@gmail.com>
2023-07-13 13:41:13 -0700

    Changed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Prints "Programming is like building a multilingual puzzle"
+ * main - Prints "Programming is like building a multilingual puzzle
  *return - return 0 if positive 
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle, \n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
