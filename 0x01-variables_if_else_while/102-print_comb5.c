ab540090cb93361a9e033916138c6a3639d7376c
Nginacloud <nginadorcas451@gmail.com>
2023-07-14 16:00:37 -0700

    All

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two two-digit numbers.
+ * Return: 0 if positive, non-zero if otherwise
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 10; i < 100; i++)
+	{
+		for (j = 10; j < 100; j++)
+		{
+			putchar ('0' + i / 10);
+			putchar ('0' + i % 10);
+			putchar (' ');
+			putchar ('0' + j / 10);
+			putchar ('0' + j % 10);
+			if (i != 99 || j != 99)
+			{
+				putchar (',');
+				putchar (' ');
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
