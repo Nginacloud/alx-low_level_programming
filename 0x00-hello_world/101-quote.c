19c8dde9e4af9208858b2304038b6dee1717ee5a
Nginacloud <nginadorcas451@gmail.com>
2023-07-15 01:40:24 -0700

    Changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints "and that piece of art is useful"
- *  - Dora Korpar, 2015-10-19
+ * main - prints "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
  * Return: 0 if positive, non-zeroif otherwise
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
