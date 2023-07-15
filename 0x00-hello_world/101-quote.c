34f61fa6e3cfd32fda8e496bd45414abe537c9d5
Nginacloud <nginadorcas451@gmail.com>
2023-07-15 01:11:47 -0700

    Standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - prints "and that piece of art is useful" 
- * - Dora Korpar, 2015-10-19"
+ * - Dora Korpar, 2015-10-19
  * Return: 0 if positive, non-zeroif otherwise
  */
 int main(void)
 {
-	write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
