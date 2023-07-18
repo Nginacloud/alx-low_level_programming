023c0005eff6722c373cbdc95a3543e29e64bc47
Nginacloud <nginadorcas451@gmail.com>
2023-07-18 10:30:03 -0700

    putcharr changed

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - Writes a character to the standard output (stdout).
+ * @c: The character to be written.
+ *
+ * Return: On success, returns the character written. On error, returns -1.
+ */
+int _putchar(char c);
+
+#endif /* MAIN_H */
+
