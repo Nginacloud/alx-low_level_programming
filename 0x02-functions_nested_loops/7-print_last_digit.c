e708946c956a42e7ef88a9b1c00d5c1185ac898b
Nginacloud <nginadorcas451@gmail.com>
2023-07-18 14:56:03 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit of a number.
+ * @i: character.
+ *
+ * Return: value of last digit.
+ */
+int print_last_digit(int i)
+{
+	int last;
+	last = i % 10;
+	_putchar('0' + last);
+	_putchar('\n');
+	return last;
+}
