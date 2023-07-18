c862a4321a43c1e213dbfd9a5698dc736ed63cc3
Nginacloud <nginadorcas451@gmail.com>
2023-07-18 16:35:53 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers.
+ * @d: first operand.
+ * @c: second operand.
+ *
+ * Return: always success.
+ */
+int add(int d, int c)
+{
+	return (d + c);
+}
