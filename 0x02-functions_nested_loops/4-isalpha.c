178758112fa3aa7b594efc54ed423d2984eca2cb
Nginacloud <nginadorcas451@gmail.com>
2023-07-18 13:40:37 -0700

    Ascii

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _isalpha - checks for alphabets.
- * @c: character being checked
+ * @c: character being checked.
  *
- * Return: Always 0
+ * Return: 1 for alphabetic characters 0 if otherwise.
  */
 int _isalpha(int c)
 {
-	if ((c >= 97; && c <= 122;) || (c >= 65; && c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
