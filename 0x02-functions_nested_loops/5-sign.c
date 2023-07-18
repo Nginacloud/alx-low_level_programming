ff5fba06875417f5bf99f58485c23491a1ec9255
Nginacloud <nginadorcas451@gmail.com>
2023-07-18 14:13:17 -0700

    changes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,20 +6,20 @@
  * Return: Always success
  */
 int print_sign(int n)
-{       
-        if (n > 0)
-        {       
-                _putchar('+');
-                return (1);
-        }       
-        else if (n == 0)
-        {       
-                _putchar('0');
-                return (0);
-        }       
-        else
-        {       
-                _putchar('-');
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
 		return (-1);
 	}
 }
