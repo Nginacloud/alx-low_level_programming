2532f3955fa4b9455486d1997016aec29c181404
Nginacloud <nginadorcas451@gmail.com>
2023-07-18 13:01:38 -0700

    x10

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,21 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- * _putchar - Writes a character to the standard output (stdout).
- * @c: The character to be written.
- *
- * Return: On success, returns the character written. On error, returns -1.
- */
 int _putchar(char c);
-
-/**
- * print_alphabet - prints alphabet in lowercase.
- *
- * Description: prints the alphabet, in lowercase, followed by a new line.
- *
- * Return: Does not return a value (void)
- */
 void print_alphabet(void);
-
-#endif /* MAIN_H */
+void print_alphabet_x10(void);
