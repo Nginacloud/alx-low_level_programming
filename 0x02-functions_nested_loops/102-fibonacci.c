18344279629e9f042225f72f9bb7444a6dc01f2d
Nginacloud <nginadorcas451@gmail.com>
2023-07-19 02:16:11 -0700

    changes

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#define I 50
 /**
  * main - check the code
  * Retur: Always success
  */
 int main(void)
 {
-	int i = 50;
-	int fib[i];
+	int i = I;
+	int fib[I];
 	fib[0] = 0;
 	fib[1] = 1;
-	printf("Fibonacci numbers: %d, %d"; fib[0], fib[1]);
+	printf("Fibonacci numbers: %d, %d", fib[0], fib[1]);
 
 	 for (int n = 2; n < i; n++)
 	 {
