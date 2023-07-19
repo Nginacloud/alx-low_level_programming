2d7940692d96565ad38633ab46680e6b14140317
Nginacloud <nginadorcas451@gmail.com>
2023-07-19 02:38:55 -0700

    changed

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #define I 50
 /**
- * main - check the code
- * Retur: Always success
+ * main - check the code.
+ * Return: 0 Always success
  */
 int main(void)
 {
-	int i = I;
-	unsigned long int fib[I];
-	int n;
+        int i = I;
+        unsigned long int fib[I];
+        int n;
 
-	fib[0] = 1;
-	fib[1] = 2;
-	printf("%lu, %lu", fib[0], fib[1]);
+        fib[0] = 1;
+        fib[1] = 2;
+        printf("%lu, %lu", fib[0], fib[1]);
 
-	 for (n = 2; n < i; n++)
-	 {
-		 fib[n] = fib[n - 1] + fib[n - 2];
-		 printf(", %lu", fib[n]);
-	 }
-	 printf("\n");
-	 return (0);
+         for (n = 2; n < i; n++)
+         {
+                 fib[n] = fib[n - 1] + fib[n - 2];
+                 printf(", %lu", fib[n]);
+         }
+         printf("\n");
+         return (0);
 }
