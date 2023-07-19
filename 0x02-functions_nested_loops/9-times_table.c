625997cf220d974e9863a1c3c110abb669c9c302
Nginacloud <nginadorcas451@gmail.com>
2023-07-18 17:09:58 -0700

    changed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,9 +9,9 @@ void times_table(void)
 	int j;
 	int mul;
 
-	for (i = 0; i < 10; i++)
+	for (j = 0; j < 10; j++)
 	{
-		for (j = 0; j < 10; j++)
+		for (i = 0; i < 10; i++)
 		{
 			mul = i * j;
 			if (mul < 10)
