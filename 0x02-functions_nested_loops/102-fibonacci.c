83fc7e31b1b6faca45ec0d5c642b2c0bf2e9f9bc
Nginacloud <nginadorcas451@gmail.com>
2023-07-19 02:41:07 -0700

    correction

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,7 +13,6 @@ int main(void)
         fib[0] = 1;
         fib[1] = 2;
         printf("%lu, %lu", fib[0], fib[1]);
-
          for (n = 2; n < i; n++)
          {
                  fib[n] = fib[n - 1] + fib[n - 2];
