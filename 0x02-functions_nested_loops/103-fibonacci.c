d85072c0c66a7df9dd20b4074176766527e5fa5d
Nginacloud <nginadorcas451@gmail.com>
2023-07-19 03:15:43 -0700

    fibonaccia extended

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define I 4000000
+/**
+ * main - check code.
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i = I;
+	unsigned long int fib[I];
+	int n;
+	unsigned long int sum = 0;
+
+	fib[0] = 1;
+	fib[1] = 2;
+	printf("%lu, %lu", fib[0], fib[1]);
+	for (n = 2; n < i; n++)
+	{
+		fib[n] = fin[n - 1] + fin[n - 2];
+		if (fib[n] % 2 == 0 && fib[n] <= I)
+		{
+			sum =+ fi[n]
+		}
+		else if (fib[n] > I)
+		{
+			break;
+		}
+		printf(", %lu", fib[n]);
+	}
+	printf("\n");
+	printf("%lu\n", sum);
+	return (0);
+}
