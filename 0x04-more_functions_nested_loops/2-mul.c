708d711922b6c304e549ea4c38cf558d7b7881d7
Nginacloud <nginadorcas451.gmail.com>
2023-07-20 18:58:41 +0000

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers.
+ * @a: first character
+ * @b: second character
+ *
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
