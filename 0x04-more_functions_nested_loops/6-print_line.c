40cd28ba3ddb679f3a3e2bd2cb4e7bce3668173f
Nginacloud <nginadorcas451.gmail.com>
2023-07-20 22:16:01 +0000

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: times the character _ should be printed
+ *
+ * Return: 0 success
+ */
+void print_line(int n)
+{
+	int i;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
