b5883a60d14829eae44a3bc71f1bf3188a41b4a1
Nginacloud <nginadorcas451.gmail.com>
2023-07-20 20:46:58 +0000

    print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,11 @@
  */
 void print_numbers(void)
 {
-	int ch;
+	int number;
 
-	for (ch = '0'; ch <= '9'; ch++)
+	for (number = '0'; number <= '9'; number++)
 	{
-		_putchar(ch);
+		_putchar(number);
 	}
 	_putchar('\n');
 }
