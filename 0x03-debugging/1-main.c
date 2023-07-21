3884ade45c563bae74133edc8d537947a0c9fb80
Nginacloud <nginadorcas451.gmail.com>
2023-07-21 20:44:42 +0000

    like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,12 +13,12 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
-	{
-		// putchar(i); // Comment out this line
-		i++; // Increment i to avoid infinite loop
-	}
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
