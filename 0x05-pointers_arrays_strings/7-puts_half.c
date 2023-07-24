802d111549a66415a614de5ab9fa7a61d89c022b
Nginacloud <nginadorcas451.gmail.com>
2023-07-24 20:26:05 +0000

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: pointer
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i;
+	int j;
+
+	for (j = 0; str[j] != 0; j++)
+	{
+
+	}
+	for (i = 0; i > (j / 2); i++)
+	{
+		str[i];
+	}
+}
