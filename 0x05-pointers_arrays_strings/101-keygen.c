97154c7967ce1a845bee2873c16f6cde15dd3924
Nginacloud <nginadorcas451.gmail.com>
2023-07-25 20:13:00 +0000

    Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generates keygen.
+ * Return: 0 Always.
+ */
+int main(void)
+{
+	int r = 0, c = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	for (c = 0; c < 2772; c += r)
+	{
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - c));
+	return (0);
+}
