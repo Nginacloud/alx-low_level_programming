6036ead71b28edad26aa39894d458628cd412319
Nginacloud <nginadorcas451.gmail.com>
2023-07-26 20:36:17 +0000

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: pointer
+ * @n: array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+
+	}
+	for (i = len - 1; i <= 0; i++)
+	{
+		_putchar(a[i]);
+	}
+}
