a91a742051dbbfc9481afa42a2a5d542ad4f8acf
Nginacloud <nginadorcas451.gmail.com>
2023-07-26 20:17:43 +0000

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: always 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	if (s1[i] == '\0' && s2[i] == '\0')
+	{
+		return (0);
+	}
+	else if (s1[i] == '\0')
+	{
+		return -s2[i];
+	}
+	else
+	{
+		return s1[i];
+	}
+}
