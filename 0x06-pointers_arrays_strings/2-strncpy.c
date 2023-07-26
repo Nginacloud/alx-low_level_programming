6aa9bcb09c7a1f7e9a1c01b26e152014cf0446c5
Nginacloud <nginadorcas451.gmail.com>
2023-07-26 19:47:35 +0000

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: pointer
+ * @src: string being appended
+ * @n: character
+ *
+ * Return: dest/
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
