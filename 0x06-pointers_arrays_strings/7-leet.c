9ce6e1eb5f87b13b33edab818e0d2b770f59ec97
Nginacloud <nginadorcas451.gmail.com>
2023-07-26 22:29:11 +0000

    leet codes

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,14 +9,14 @@ char *leet(char *str)
 {
 	int i;
 	int j;
-	char leet_chars = "aAeEoOtTlL";
-	char leet_codes = "4433007711";
+	char leet_chars[] = "aAeEoOtTlL";
+	char leet_codes[] = "4433007711";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; leet_char[j] != '\0'; j++)
 		{
-			if (str[i] = leet_char[j])
+			if (str[i] == leet_char[j])
 			{
 				str[i] = leet_code[j];
 				break;
