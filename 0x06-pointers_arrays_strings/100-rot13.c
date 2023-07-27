27a231ee88daf36965cfa8d23bfc8d90c1dfb3d6
Nginacloud <nginadorcas451.gmail.com>
2023-07-27 17:58:37 +0000

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @str: pointer
+ *
+ * Return: always 0
+ */
+char *rot13(char *str)
+{'
+	int i;
+	int j;
+	char data[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == data[j])
+			{
+				str[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
