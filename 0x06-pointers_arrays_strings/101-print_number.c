948ee236235af1868730c8a5132ee0fc8a98ba0a
Nginacloud <nginadorcas451.gmail.com>
2023-07-27 18:50:56 +0000

    integers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,6 +7,11 @@
  */
 void print_number(int n)
 {
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 	if (n < 0)
 	{
 		_putchar('-');
@@ -14,7 +19,7 @@ void print_number(int n)
 	}
 	if (n / 10 != 0)
 	{
-		print_number(n / 10);
+		print-number(n / 10);
 	}
 	_putchar((n % 10) + '0');
 }
