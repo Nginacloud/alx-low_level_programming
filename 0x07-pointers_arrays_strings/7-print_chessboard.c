f976e9e4e4acbf819b3e423d5a1c35e2b4c0dfe9
Nginacloud <nginadorcas451.gmail.com>
2023-07-31 21:33:09 +0000

    chessboard2

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,10 +12,15 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j < 8; j++)
+		if (i % 8 == 0 && i != 0)
 		{
-			_putchar(a[i][j]);
+			for (j = 0; j < i + 8; j++)
+			{
+				_putchar(a[j / 8][j % 8]);
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
+		_putchar(a[i / 8][i % 8]);
 	}
+	_putchar('\n');
 }
