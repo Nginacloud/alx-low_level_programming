f5c4e94db6f68d22b244163a690e5aa29c8e1242
Nginacloud <nginadorcas451.gmail.com>
2023-07-31 22:15:17 +0000

    diagsums2

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,15 +10,12 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, n, s1 = 0, s2 = 0;
+	int i, s1 = 0, s2 = 0;
 
-	for (i = 0; i < (size * size); i++)
+	for (i = 0; i < size ; i++)
 	{
-		s1 = s1 + a[i];
-	}
-	for (n = size - 1; n <= (size * size) - size; n = n + size -1)
-	{
-		s2 = s2 + a[n];
+		s1 += a[i * size + i];
+		s2 += a[i * size + (size - 1 - i)];
 	}
 	printf("%d, %d\n", s1, s2);
 }
