c52952966438c8ced2dc6f298d94430e1ce6385e
Nginacloud <nginadorcas451.gmail.com>
2023-07-31 19:16:02 +0000

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * int_strspn - gets the length of a prefix substring
+ * @s: string pointer
+ * @accept: bytes
+ *
+ * Return: number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, n, value, check;
+
+	value = 0;
+
+	for (i =  0; s[i] != '\0'; i++)
+	{
+		check = 0;
+
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (accept[n] == s[i])
+			{
+				value++;
+				check = 1;
+			}
+		}
+		return (check == 0);
+	}
+	return (value);
+}
