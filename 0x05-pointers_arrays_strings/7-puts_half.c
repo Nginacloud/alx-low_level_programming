09e477323f3d8ad6dc81db9a5baf55b4efa4202d
Nginacloud <nginadorcas451.gmail.com>
2023-08-01 09:05:28 +0000

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,13 +8,13 @@
 void puts_half(char *str)
 {
 	int i = 0;
-	int j = *str;
+	int j;
 
 	for (j = 0; str[j] != '\0'; j++)
 	{
 
 	}
-	for (i = (j / 2); i < j; i += 1)
+	for (i = ((j - 1) / 2) + 1; i < j; i += 1)
 	{
 		_putchar(str[i]);
 	}
