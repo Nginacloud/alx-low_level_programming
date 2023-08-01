0f6cf2a3b5096591da239a7a3f273d7659f1bb6e
Nginacloud <nginadorcas451.gmail.com>
2023-08-01 08:00:31 +0000

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * int_strspn - gets the length of a prefix substring
+ * _strspn - gets the length of a prefix substring
  * @s: string pointer
  * @accept: bytes
  *
