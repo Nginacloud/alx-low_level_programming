9dfff559efd912ce7c7086f1d3a4013f8f2351b2
Nginacloud <nginadorcas451.gmail.com>
2023-08-02 20:24:53 +0000

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _sqrt_recursion - natural squareroot of a number
+ * @n: integer
+ *
+ * Return: always 0
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
