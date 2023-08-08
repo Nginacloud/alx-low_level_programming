6ad3752451b8bfe1ad2fb62366f08651bae5a312
Nginacloud <nginadorcas451.gmail.com>
2023-08-08 20:47:08 +0000

    grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,7 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: column
+ * @height: row
  *
+ * Return: NULL on fail
  */
 int **alloc_grid(int width, int height)
 {
