e08babb8ecd2f45464c14bff0baba2ad89ddd22e
Nginacloud <nginadorcas451.gmail.com>
2023-08-08 21:00:24 +0000

    4-free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * free_grid - frees a 2 dimensional grid 
- * @grid: 
+ * free_grid - frees a 2 dimensional grid
+ * @grid: pointer
  * @height: row
  *
  * Return: success
