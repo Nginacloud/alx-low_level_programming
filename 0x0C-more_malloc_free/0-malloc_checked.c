98ed741720ac5ea447d4e56aa1b013ddb296cbd5
Nginacloud <nginadorcas451.gmail.com>
2023-08-10 18:09:52 +0000

    malloc memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_cheched - allocates memory using malloc
+ * @b: size of memory
+ *
+ * Return: exit (98) incase it fails, else pointer
+ */
+void *malloc_checked(unsigned int b);
+{
+	void (*ptr = malloc(b));
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return ptr;
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+0x0C-more_malloc_free
+More concept on automatic and dynamic allocation
