583479a21c9e8a6c3931e863ad2d77fdc128c84b
Nginacloud <nginadorcas451.gmail.com>
2023-08-10 19:01:40 +0000

    concatenated two strings and added function to main

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,3 +3,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 int _putchar(char c);
+unsigned int _strlen(char *s);
+
