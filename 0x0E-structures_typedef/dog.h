c48c2678894ee34364d702ac0fe703a56cfd219d
Nginacloud <nginadorcas451.gmail.com>
2023-08-14 07:02:33 +0000

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - basic dog information
+ * @name: first
+ * @age: second
+ * @owner: third
+ *
+ * Description: defination of struct dog type
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
