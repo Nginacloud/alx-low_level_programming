f7fd7598d275fb1c371d4747c04b1d5588bafac1
Nginacloud <nginadorcas451.gmail.com>
2023-08-16 15:42:57 +0000

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
-#include "function_pointer.h"
+#include "function_pointers.h"
 /**
  * print_name - print name using pointer to function
  * @name: string
  * @f: pointer to funtion
  *
- * Return: nothing
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
