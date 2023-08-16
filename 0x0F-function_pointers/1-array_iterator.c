91bf87a82fac0cf028e5ff7e175b4b101e39b0fc
Nginacloud <nginadorcas451.gmail.com>
2023-08-16 07:46:05 +0000

    iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "funtion_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - prints each array on new line
+ * @array: array
+ * @size: number of elements to print
+ * @action: pointer
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
