f26795ecc9ff303274a32379a4533f8160ab5568
Nginacloud <nginadorcas451.gmail.com>
2023-08-17 22:18:18 +0000

    3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,43 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_all - prints anything
+ * @format: list types of arguments
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	char *str;
+	char curr_format;
+
+	va_start(args, format);
+	for (unsigned int i = 0; format && format[i]; ++i)
+	{
+		curr_format = format[i];
+		if (i > 0)
+		{
+			printf(", ");
+		}
+		switch (curr_format)
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char*);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				break;
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
