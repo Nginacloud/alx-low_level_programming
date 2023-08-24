ade978b24fd3d397c07275100926f63499c8abdc
Nginacloud <nginadorcas451.gmail.com>
2023-08-24 21:05:57 +0000

    added prototypes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,6 +19,8 @@ typedef struct list_s
 #include <stddef.h>
 int _putchar(char c);
 size_t print_list(const list_t *h);
+void first_func_print(void) __attribute__((constructor));
+void first_func_print(void);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
