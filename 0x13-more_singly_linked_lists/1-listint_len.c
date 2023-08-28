ec5f5946746fb845dc53c5e270b45c9de217c492
Nginacloud <nginadorcas451.gmail.com>
2023-08-28 20:57:00 +0000

    number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stddef.h>
+/**
+ * listint_len - returns number of elements
+ * @h: pointer
+ *
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
