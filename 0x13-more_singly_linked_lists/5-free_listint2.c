ddf4fd59df5d558e3fc7bcc327dfca3650f8f53e
Nginacloud <nginadorcas451.gmail.com>
2023-08-29 18:10:42 +0000

    sets head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - sets head to null
+ * @head: pointer
+ *
+ * Return: 0
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;/*update head*/
+		free(current);
+	}
+}
