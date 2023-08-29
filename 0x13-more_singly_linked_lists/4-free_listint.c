a3e27e6116e9528e430a16cb4029a898fcdc222d
Nginacloud <nginadorcas451.gmail.com>
2023-08-29 17:45:26 +0000

    free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint - frees list
+ * @head: pointer
+ *
+ * Return: 0
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = heah->next;
+		free(current);
+	}
+}
